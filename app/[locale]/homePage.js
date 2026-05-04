feaffce9125ddfacb99227ce86da2074cf960c65
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2026-05-04 13:37:14 +0400

    sssasa

--- app/[locale]/homePage.js
+++ app/[locale]/homePage.js
@@ -65,9 +65,7 @@ export default function HomePage() {
         <div id="liquidity-solutions" className="scroll-mt-36">
           <LiquiditySolutions />
         </div>
-        <div id="smart-trading" className="scroll-mt-36">
-          <TradingOptionsSection />
-        </div>
+       
         <SecurityFund />
         <div id="account-types" className="scroll-mt-36">
           <TradingPlatform />
